88764207ef020f0f0c15739c57c54b3a9c48f17f
Samuel <amoskevinsamuel@yahoo.com>
2023-01-11 09:06:30 +0100

    Completed

---
+++ landing-page-razor/Pages/Demo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace landing_page_razor.Pages
+{
+    public class DemoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
