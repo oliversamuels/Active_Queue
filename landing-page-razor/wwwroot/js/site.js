74bbe69831bee983a5ce8a2bbf6af2cb04909951
Samuel <amoskevinsamuel@yahoo.com>
2023-02-01 10:21:25 +0100

    Responsiveness

--- landing-page-razor/wwwroot/js/site.js
+++ landing-page-razor/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿/*News Script*/
+let items = document.querySelectorAll(".nav-item");
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        const viewId = item.dataset.id;
+        const viewItem = document.getElementById(viewId);
+        const currentViewItem = document.querySelector('.show');
+        const activeLink = document.querySelector('.active');
+        activeLink.classList.remove("active");
+        item.classList.add("active");
+        currentViewItem.classList.remove("show");
+        viewItem.classList.add("show");
+    })
+})
